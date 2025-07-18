5f066d8e511f9c1bb36e0de7e4fdd496537c2063
lllDavid <imperfectrising5527@gmail.com>
2025-07-18 20:27:44 +0200

    initial commit

---
+++ src/main/java/com/ehrblockchain/EhrBlockchainApplication.java
@@ -0,0 +1,14 @@
+package com.ehrblockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class EhrBlockchainApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(EhrBlockchainApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ehrblockchain/EhrblockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehrblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrblockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
